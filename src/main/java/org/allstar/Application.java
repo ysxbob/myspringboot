c1994a9a221891e2113c9840005de5f1d361c53d
ysxbob <brilliant_ysx@163.com>
2016-06-13 23:16:58 +0800

    0613

--- src/main/java/org/allstar/Application.java
+++ src/main/java/org/allstar/Application.java
@@ -1,15 +1,54 @@
 package org.allstar;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.mangofactory.swagger.configuration.SpringSwaggerConfig;
+import com.mangofactory.swagger.models.dto.ApiInfo;
+import com.mangofactory.swagger.plugin.EnableSwagger;
+import com.mangofactory.swagger.plugin.SwaggerSpringMvcPlugin;
 
 /**
  * Application
  *
  */
-@SpringBootApplication // same as @Configuration @EnableAutoConfiguration @ComponentScan
-class Application  {
-  public static void main(String[] args) {
-	SpringApplication.run(Application.class,args);
-}
+@EnableSwagger
+@SpringBootApplication
+// same as @Configuration @EnableAutoConfiguration @ComponentScan
+class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	private SpringSwaggerConfig springSwaggerConfig;
+
+	/**
+	 * Required to autowire SpringSwaggerConfig
+	 */
+	@Autowired
+	public void setSpringSwaggerConfig(SpringSwaggerConfig springSwaggerConfig) {
+		this.springSwaggerConfig = springSwaggerConfig;
+	}
+
+	/**
+	 * Every SwaggerSpringMvcPlugin bean is picked up by the swagger-mvc
+	 * framework - allowing for multiple swagger groups i.e. same code base
+	 * multiple swagger resource listings.
+	 */
+	@Bean
+	public SwaggerSpringMvcPlugin customImplementation() {
+		return new SwaggerSpringMvcPlugin(this.springSwaggerConfig).apiInfo(
+				apiInfo()).includePatterns(".*?");
+	}
+
+	private ApiInfo apiInfo() {
+		ApiInfo apiInfo = new ApiInfo(
+				"李威测试使用的页面接口文档", // App Service API
+				"副标题", "111111111111111111111111", "121088825@qq.com",
+				"2222222222222222222222222222", "3333333333333333333333333333");
+		return apiInfo;
+	}
+
 }
